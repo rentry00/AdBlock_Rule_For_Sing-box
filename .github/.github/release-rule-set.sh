7793a05c59380c1cab0bb31a51c71974a39c675a
rentry00 <108783040+rentry00@users.noreply.github.com>
2025-07-13 13:06:21 +0800

    Update release-rule-set.sh
    Signed-off-by: rentry00 <108783040+rentry00@users.noreply.github.com>

--- .github/.github/release-rule-set.sh
+++ .github/.github/release-rule-set.sh
@@ -5,20 +5,15 @@
 set -e -o pipefail
 
 
-wget https://github.com/SagerNet/sing-box/releases/download/v1.11.0/sing-box-1.11.0-linux-amd64.tar.gz
-tar -zxvf sing-box-1.11.0-linux-amd64.tar.gz
+wget https://github.com/SagerNet/sing-box/releases/download/v1.11.15/sing-box-1.11.15-linux-amd64.tar.gz
+tar -zxvf sing-box-1.11.15-linux-amd64
 
-for file in output/*.json; do
-    sing-box-1.11.0-linux-amd64/sing-box rule-set compile "$file"
-done
+sing-box-1.11.15-linux-amd64/sing-box rule-set compile --output adblock_reject2.srs adblock_reject2.json
 
 
 cd output
 git init
-git config --local user.email "github-action@users.noreply.github.com"
-git config --local user.name "GitHub Action"
-git remote add origin https://github-action:$GITHUB_TOKEN@github.com/xmdhs/sing-box-ruleset.git
-git branch -M rule-set
-git add .
-git commit -m "Update rule-set"
-git push -f origin rule-set
+git config --global user.name 'github-actions'  # 配置提交用户名
+git config --global user.email 'github-actions@github.com'  # 配置提交邮箱
+git add -f adblock_reject.json  # 强制添加 adblock_reject.json 文件
+git commit -m 'Update adblock_reject.json' || git commit --allow-empty -m 'Empty commit to force push'  # 提交更改，若无更改则提交空更改
