cb059c57922f1627be8d1a523487c600385d1e95
rentry00 <108783040+rentry00@users.noreply.github.com>
2025-07-13 13:56:34 +0800

    Update release-rule-set.sh
    Signed-off-by: rentry00 <108783040+rentry00@users.noreply.github.com>

--- .github/release-rule-set.sh
+++ .github/release-rule-set.sh
@@ -11,10 +11,12 @@ tar -zxvf sing-box-1.11.15-linux-amd64.tar.gz
 sing-box-1.11.15-linux-amd64/sing-box rule-set compile --output adblock_reject2.srs adblock_reject2.json
 
 git init
-git config --global user.name 'github-actions'  # 配置提交用户名
-git config --global user.email 'github-actions@github.com'  # 配置提交邮箱
+git config --local user.email "github-action@users.noreply.github.com"
+git config --local user.name "GitHub Action"
 git remote add origin https://github-action:$GITHUB_TOKEN@github.com/rentry00/AdBlock_Rule_For_Sing-box.git
 git branch -M main
 git add .
-git commit -m 'Update adblock_reject2.srs' || git commit --allow-empty -m 'Empty commit to force push'  # 提交更改，若无更改则提交空更改
+git commit -m 'Update adblock_reject2.srs' || git commit --allow-empty -m 'Empty commit to force push'  # 提交更改，若无更改则提交空更
 git push -f origin main
+
+
